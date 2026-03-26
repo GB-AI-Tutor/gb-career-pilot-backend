d4f147a30474c09bcc0d1434ae7686fabd86fed4
Raqeeb Raees <143945205+RaeesRaqeeb@users.noreply.github.com>
2026-03-26 07:33:24 +0500

    Update universities.py

--- src/schemas/universities.py
+++ src/schemas/universities.py
@@ -92,6 +92,7 @@ class UniversityUpdate(BaseModel):
     # programs : list[ProgramBase] | None = Field(default=None)
 
 
+
 class UniversitySearchArgs(BaseModel):
     location: str | None = Field(
         None, description=" The city or region to search in (e.g, 'Lahore', 'Gilgit')"
@@ -100,7 +101,7 @@ class UniversitySearchArgs(BaseModel):
         None, description="The Subject or Degree field(e.g., 'Computer Science' ,'Nursing)"
     )
 
-    max_fee: int | None = Field(
+    max_fee: int | str | None = Field(
         None, description=" The Maximum annual tuition fee the student can afford."
     )
 
